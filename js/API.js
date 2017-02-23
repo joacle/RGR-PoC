a5b04f59f8353f9d1ca14e4858b6a6fc92ce7d8f
jofe <joaquin.ferraz@spenta.es>
2017-02-23 12:06:46 +0100

    Setup Flux

--- js/API.js
+++ js/API.js
@@ -1,10 +1,11 @@
 import {get} from 'jquery';
+import ServerActions from './actions/ServerActions';
 
 let API = {
     fetchLinks() {
         console.log('1. In API');
         get('/data/links').done(resp => {
-            console.log(resp);
+            ServerActions.receiveLinks(resp);
         })
     }
 }

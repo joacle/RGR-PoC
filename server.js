f7a47e22827dec9fd5cb18f237fd1f0b0ed2f998
joacle <joaquinfs@hotmail.com>
2017-02-23 15:45:26 +0100

    Setup GraphQL

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import schema from './data/schema';
+import GraphQLHTTP from 'express-graphql';
 import { MongoClient } from 'mongodb';
 
 let MONGO_URL="mongodb://rgrpoc:rgrpoc@ds055555.mlab.com:55555/rgr-poc";
@@ -8,21 +9,18 @@ let app = express();
 
 app.use(express.static('public'));
 
-
 let db;
 
 MongoClient.connect(MONGO_URL, (err, database) => {
     if (err) throw err;
 
     db = database;
+
+    app.use('/graphql', GraphQLHTTP({
+        schema: schema(db),
+        graphiql: true
+    }))
+
     app.listen(3000, () => console.log('Listening on port 3000'));
 
-});
-
-app.get('/data/links', (req, res) => {
-    db.collection('links').find({}).toArray((err, links) => {
-        if (err) throw err;
-
-        res.json(links);
-    })
 });

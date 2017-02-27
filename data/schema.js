77ad1fa1926e13c10be36f48685bbfabde42ba1b
joacle <joaquinfs@hotmail.com>
2017-02-27 16:38:46 +0100

    Add sorting and optimistic response

--- data/schema.js
+++ data/schema.js
@@ -27,7 +27,9 @@ let Schema= (db) => {
                 type: linkConnection.connectionType,
                 args: connectionArgs,
                 resolve: (_, args) => connectionFromPromisedArray(
-                    db.collection('links').find({}).limit(args.first).toArray(),
+                    db.collection('links').find({})
+                    .sort({createdAt: -1})
+                    .limit(args.first).toArray(),
                     args
                 )
             }
@@ -43,7 +45,11 @@ let Schema= (db) => {
                 resolve: (obj) => obj._id
             },
             title: { type: GraphQLString },
-            url: { type: GraphQLString }
+            url: { type: GraphQLString },
+            createdAt: {
+                type: GraphQLString,
+                resolve: (obj) => new Date(obj.createdAt).toISOString()
+            }
         })
     });
 
@@ -71,7 +77,11 @@ let Schema= (db) => {
         },
 
         mutateAndGetPayload: ({title, url}) => {
-            return db.collection('links').insertOne({title, url});
+            return db.collection('links').insertOne({
+                title, 
+                url,
+                createdAt: Date.now()
+            });
         }
     });
 
--- js/components/Main.js
+++ js/components/Main.js
@@ -2,12 +2,26 @@ import React from 'react';
 import Relay from 'react-relay';
 
 import Link from './Link';
+import CreateLinkMutation from '../mutations/CreateLinkMutation';
 
 class Main extends React.Component{
     setLimit = (e) => {
         let newLimit = Number(e.target.value);
         this.props.relay.setVariables({ limit: newLimit });
     }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        Relay.Store.update(
+            new CreateLinkMutation({
+                title: this.refs.newTitle.value,
+                url: this.refs.newUrl.value,
+                store: this.props.store
+            })
+        );
+        this.refs.newTitle.value = '';
+        this.refs.newUrl.value = ''
+    }
+
     static propTypes = {
         limit: React.PropTypes.number
     }
@@ -23,9 +37,16 @@ class Main extends React.Component{
         return (
             <div>
                 <h3>Links</h3>
-                Showing: <select onChange={this.setLimit}>
-                    <option value="2">2</option>
-                    <option value="5" selected>5</option>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Title" ref="newTitle" />
+                    <input type="text" placeholder="Url" ref="newUrl" />
+                    <button type="submit">Add</button>
+                </form>
+                Showing:&nbsp;
+                <select onChange={this.setLimit} 
+                                defaultValue={this.props.relay.variables.limit}>
+                    <option value="100">100</option>
+                    <option value="200">200</option>
                 </select>
                 <ul>
                     {content}
@@ -37,11 +58,12 @@ class Main extends React.Component{
 
 Main = Relay.createContainer(Main, {
     initialVariables: {
-        limit: 5
+        limit: 100
     },
     fragments: {
         store: () => Relay.QL`
             fragment on Store {
+                id,
                 linkConnection(first: $limit) {
                     edges {
                         node {
